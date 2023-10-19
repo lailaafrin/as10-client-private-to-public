5e3b3d2b00c42f3a93b3d378433adf413353dfc5
laila-afrin <laila.afrin80@gmail.com>
2023-10-19 22:30:38 +0600

    carusul with brand

--- src/sheard/Product1.jsx
+++ src/sheard/Product1.jsx
@@ -1,11 +1,32 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
+
+
+
 
 const Product1 = () => {
     return (
 
-        <div>
+        <div className='h-42 '>
+            <Carousel className=' text-center'>
+                
+                    <img src="https://i.ibb.co/GtjMrZ8/Photography-PRL-Image-6-1.webp" />
+
+
+                    <img src="https://i.ibb.co/yV84Qby/brand5.jpg" />
+
             
-        </div>
+                
+                    <img src="https://i.ibb.co/mD87Zxv/brand3.jpg" />
+
+                
+
+
+
+            </Carousel>
+       </div>
     );
 };
 
